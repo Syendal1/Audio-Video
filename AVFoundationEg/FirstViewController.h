a43937880664833fbf547992240a35d38087aa72
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-03 23:31:51 -0700

    Initial Commit

---
+++ AVFoundationEg/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  AVFoundationEg
+//
+//  Created by SnehithNitin on 8/3/16.
+//  Copyright © 2016 Snehith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ AVFoundationEg/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  AVFoundationEg
+//
+//  Created by SnehithNitin on 8/3/16.
+//  Copyright © 2016 Snehith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+

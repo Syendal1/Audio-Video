d3f8ff951b58c2e9fcadfbb2345498d84382c2a9
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-04 10:36:13 -0700

    Audio Done

--- AVFoundationEg/FirstViewController.m
+++ AVFoundationEg/FirstViewController.m
@@ -6,18 +6,54 @@
 //  Copyright © 2016 Snehith. All rights reserved.
 //
 
+#include <AVFoundation/AVFoundation.h>
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
-
+@interface FirstViewController (){
+    UIView *colorView1;
+    AVAudioPlayer *player;
+}
 @end
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    colorView1=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    colorView1.backgroundColor=[UIColor purpleColor];
+    [self.view addSubview:colorView1];
+    
+    UIButton *playAudioBtn=[[UIButton alloc]initWithFrame:CGRectMake(150, 100, 100, 100)];
+    [playAudioBtn setTitle:@"Play Audio" forState:UIControlStateNormal];
+    playAudioBtn.backgroundColor=[UIColor greenColor];
+    [colorView1 addSubview:playAudioBtn];
+    
+    UIButton *stopAudioBtn=[[UIButton alloc]initWithFrame:CGRectMake(150, 350, 100, 100)];
+    [stopAudioBtn setTitle:@"Stop Audio" forState:UIControlStateNormal];
+    stopAudioBtn.backgroundColor=[UIColor redColor];
+    [colorView1 addSubview:stopAudioBtn];
+    
+    [playAudioBtn addTarget:self action:@selector(playAudioMethod) forControlEvents:UIControlEventTouchUpInside];
+    [stopAudioBtn addTarget:self action:@selector(stopAudioMethod) forControlEvents:UIControlEventTouchUpInside];
+    ///**************//
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"SaturdaySong" ofType:@"mp3"];
+    NSURL *soundURL = [NSURL fileURLWithPath:path];
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
+    ////////*********////
 }
+
+-(void)playAudioMethod{
+    colorView1.backgroundColor=[UIColor yellowColor];
+    [player prepareToPlay];
+    [player play];
+}
+
+-(void)stopAudioMethod{
+    colorView1.backgroundColor=[UIColor purpleColor];
+    [player stop];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- AVFoundationEg/SecondViewController.m
+++ AVFoundationEg/SecondViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "SecondViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
 
-@interface SecondViewController ()
+
+@interface SecondViewController (){
+    UIView *colorView2;
+    AVPlayer *avPlayer;
+    AVPlayerViewController *avPlayerViewController;
+}
 
 @end
 
@@ -16,8 +23,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    colorView2=[[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    colorView2.backgroundColor=[UIColor purpleColor];
+    [self.view addSubview:colorView2];
+    
+    UIButton *playVideoBtn=[[UIButton alloc]initWithFrame:CGRectMake(150, 100, 100, 100)];
+    [playVideoBtn setTitle:@"Play Video" forState:UIControlStateNormal];
+    playVideoBtn.backgroundColor=[UIColor greenColor];
+    [colorView2 addSubview:playVideoBtn];
+    [playVideoBtn addTarget:self action:@selector(playVideoMethod) forControlEvents:UIControlEventTouchUpInside];
+    
+//    NSString *path = [[NSBundle mainBundle] pathForResource:@"VideoFileName" ofType:@"mp4"];
+//    NSURL *videoURL = [NSURL fileURLWithPath:path];
+
+//    avPlayer=[AVPlayer playerWithURL:videoURL];
+//    avPlayerViewController.player=avPlayer;
+
+    
 }
+
+//-(void)playVideoMethod{
+//    colorView2.backgroundColor=[UIColor yellowColor];
+//    [self presentViewController:avPlayerViewController animated:true completion:^{
+//        //[avPlayerViewController player];
+//        //[self avPlayerViewController.player.play ];
+//        //avPlayerViewController.player.play();
+//    }];
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
